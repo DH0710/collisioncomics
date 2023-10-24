a3e4ab09281b08cb4db7afa373fd42b6409f810c
blur <damienhebert710@gmail.com>
2023-10-23 20:19:05 -0500

    creating server, setting up node and express.js

---
+++ server/index.js
@@ -0,0 +1,32 @@
+require("dotenv").config();
+// you need CORS to fetch data
+const cors = require("cors");
+const express = require("express");
+
+
+const app = express();
+const PORT = process.env.PORT || 8000;
+
+
+
+app.use(cors());
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
+
+
+app.get("/", (req, res) => {
+    res.json("Hello Damien your server is running now!")
+});
+
+app.get("*", (req, res) => {
+    res.sendStatus("404")
+});
+
+
+
+
+
+app.listen(PORT, () => {
+    console.log(`Server is running on Port: ${PORT}`);
+});
+
