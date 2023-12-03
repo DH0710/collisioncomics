50eaf4faa9dbddf0cb34795b0547aa77bd2bc093
blur <damienhebert710@gmail.com>
2023-12-03 04:28:44 -0600

    creating different inputs for creating comic such as picture, description, title, etc

--- client/src/routes/Comics/createComic.jsx
+++ client/src/routes/Comics/createComic.jsx
@@ -5,8 +5,13 @@ const createComic = () => {
 
     const [title, setTitle] = useState("");
     const [slug, setSlug] = useState("");
+    const [stars, setStars] = useState(0);
+    const [description, setDescription] = useState("");
+    const [categories, setCategories] = useState([]);
+    const [thumbnail, setThumbnail] = useState(null);
+    const [submitted, setSubmitted] = useState("");
 
-    const createComic = async(e) => {
+    const createComic = async (e) => {
 
         e.preventDefault();
         console.table([title, slug]);
@@ -14,20 +19,22 @@ const createComic = () => {
         try {
             const response = await fetch("http://localhost:8000/api/comics", {
                 method: "POST",
-                headers: {"Content-Type": "application/json"},
+                headers: { "Content-Type": "application/json" },
                 body: JSON.stringify(
-                {
-                    title: title,
-                    slug: slug,
+                    {
+                        title: title,
+                        slug: slug,
 
-                }
+                    }
                 )
 
             })
 
-            if(response.ok) {
+            if (response.ok) {
                 setTitle("");
                 setSlug("");
+                setSubmitted(true);
+
             } else {
                 console.log("Failed to submit data")
             }
@@ -52,7 +59,7 @@ const createComic = () => {
     }
 
 
-    
+
 
 
 
@@ -65,55 +72,81 @@ const createComic = () => {
                 data below is pulled from MongoDB database.
             </p>
 
-            <form className='comicdetails' onSubmit={createComic}>
-                <div className='col-1'>
-                    <label> Upload Thumbnail</label>
-                    <img src={NoImagePlaceholder} alt="preview image"></img>
+            {submitted ? (
 
-                    <input type="file" accept="image/gif, image/jpeg, image/png" />
+                <p> Data submitted successfully!</p>
+            ) : (
 
+                <form className='comicdetails' onSubmit={createComic}>
+                    <div className='col-1'>
+                        <label> Upload Thumbnail</label>
+                        <img src={NoImagePlaceholder} alt="preview image"></img>
 
+                        <input type="file" accept="image/gif, image/jpeg, image/png" />
 
-                </div>
-
-                <div className='col-2'>
-
-                    <div>
-                        <label> Title </label>
-                        <input
-                            type="text"
-                            value={title}
-                            onChange={(e) => setTitle(e.target.value)}>
-                        </input>
+                    </div>
 
 
-                        <label> Slug </label>
-                        <input
-                            type="text"
-                            value={slug}
-                            onChange={(e) => setSlug(e.target.value)}>
-                        </input>
+
+                    <div className='col-2'>
+
+                        <div>
+                            <label> Title </label>
+                            <input
+                                type="text"
+                                value={title}
+                                onChange={(e) => setTitle(e.target.value)}>
+                            </input>
+
+
+                            <label> Slug </label>
+                            <input
+                                type="text"
+                                value={slug}
+                                onChange={(e) => setSlug(e.target.value)}>
+                            </input>
+
+                            <label> Stars </label>
+                            <input
+                                type="text"
+                                value={stars}
+                                onChange={(e) => setStars(e.target.value)}>
+                            </input>
+
+                            <label> Description </label>
+                            <textarea
+                               
+                                value={description}
+                                onChange={(e) => setDescription(e.target.value)}>
+                            </textarea>
+
+                            <label> Categories </label>
+                            <input
+                                type="text"
+                                value={}
+                                onChange={(e) => setCategories(e.target.value)}>
+                            </input>
+
+
+                        </div>
+
+
+                        <input type="submit"></input>
+
 
 
                     </div>
 
 
-                    <input type="submit"></input>
-
-
-
-                </div>
 
 
 
 
 
 
+                </form>
 
-
-            </form>
-
-
+            )}
 
 
 
