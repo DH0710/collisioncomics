6fba445eafdf0e30bd9d6de70036ce86b5725159
blur <damienhebert710@gmail.com>
2023-12-17 20:17:59 -0600

    new commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./routes/Home/home.jsx";
 import About from "./routes/About/about.jsx";
-import Comics from "./routes/Comics/comics.jsx"
+import Comics from "./routes/Comics/comics.jsx";
 import SingleComic from "./routes/Comics/singleComic.jsx";
 import CreateComic from "./routes/Comics/createComic.jsx";
 import EditComic from "./routes/Comics/editComic.jsx";
